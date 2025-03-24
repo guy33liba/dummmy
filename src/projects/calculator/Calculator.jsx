a777843617db78b5b5cfab9331647c1ee6e8d7c7
guy33liba <guy33liba@gmail.com>
2025-03-24 22:02:26 +0200

    fasdf

--- src/projects/calculator/Calculator.jsx
+++ src/projects/calculator/Calculator.jsx
@@ -1,23 +1,39 @@
 import React, { useState } from "react";
 import "./Calculator.css";
-
 const Calculator = () => {
  const [current, setCurrent] = useState("");
  const [previous, setPrevious] = useState("");
  const [operator, setOperator] = useState("");
-
- const numbers = ["*", 9, 8, 7, "-", 6, 5, 4, "+", 3, 2, 1, "/", ".", 0, "=", "AC", "DEL"];
-
+ const [numbers, setNumbers] = useState([
+  "*",
+  9,
+  8,
+  7,
+  "-",
+  6,
+  5,
+  4,
+  "+",
+  3,
+  2,
+  1,
+  "/",
+  ".",
+  0,
+  "=",
+  "AC",
+  "DEL",
+ ]);
  const handleNumbersAndOperators = (btn) => {
   if (btn === "AC") {
    setCurrent("");
    setPrevious("");
   } else if (btn === "DEL") {
-   const slicedCurrent = current.slice(0, -1);
-   setCurrent(slicedCurrent);
+   const slicedcurrent = current.slice(0, -1);
+   setCurrent(slicedcurrent);
   } else if (btn === "+" || btn === "-" || btn === "*" || btn === "/") {
    setOperator(btn);
-   setPrevious(current + btn);
+   setPrevious(btn + current);
    setCurrent("");
   } else if (btn === "=") {
    calculate(operator);
@@ -25,7 +41,6 @@ const Calculator = () => {
    setCurrent((prev) => prev + btn);
   }
  };
-
  const calculate = (operator) => {
   let numCurrent = parseFloat(current);
   let numPrevious = parseFloat(previous);
@@ -40,37 +55,39 @@ const Calculator = () => {
     break;
    case "/":
     if (numCurrent !== 0) {
-     result = numPrevious / numCurrent;
-    } else {
-     result = "Error";
+     result = numCurrent / numPrevious;
     }
     break;
    case "*":
     result = numCurrent * numPrevious;
     break;
-   default:
-    return;
   }
   setCurrent(result.toString());
   setPrevious("");
   setOperator("");
  };
-
  return (
   <div className="calculatorContainer">
-   <h1>מחשבון</h1>
+   <h1>מחשבון </h1>
    <div className="screen">
-    <div>
-     {previous} {operator}
-    </div>
+    <div>{previous}</div>
     <div>{current}</div>
    </div>
    <div className="numbersContainer">
-    {numbers.map((num, index) => (
-     <button key={index} className="numbers" onClick={() => handleNumbersAndOperators(num)}>
-      {num}
-     </button>
-    ))}
+    {numbers.map((num, index) => {
+     return (
+      <div key={index}>
+       <button
+        className="numbers"
+        onClick={() => {
+         handleNumbersAndOperators(num);
+        }}
+       >
+        {num}
+       </button>
+      </div>
+     );
+    })}
    </div>
   </div>
  );
